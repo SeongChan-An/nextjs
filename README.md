67c3cee2123d6f94b18318d79422b1d2f43296d6
tjscks0510 <tjscks0510@gmail.com>
2022-02-28 18:38:55 +0900

    [Seongchan, ticket-#] [Title] learn about nextjs and practice [Description] - [Relevant] - https://nextjs.org/

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# nextjs
+# NextJS Introduction
+
+## create-react-app VS cretae-next-app
+
+- create-react-app
+  : client side rendering
+  : 브라우저가 유저가 보는 UI를 만드는 모든 것을 함
+
+- next
+  : server side rendering
+
+## 더 알아보기
+
+- https://velog.io/@devstone/Next.js-100-%ED%99%9C%EC%9A%A9%ED%95%98%EA%B8%B0-feat.-initialProps-webpack-storybook
+
+- [getServerSideProps](https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props)
--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -5,26 +5,29 @@ import styles from "./NavBar.module.css";
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav>
-      <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
-      </Link>
-      <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
-      </Link>
+    <nav className={styles.nav}>
+      <img className={styles.img} src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <a
+            className={`${styles.link} ${
+              router.pathname === "/" ? styles.active : ""
+            }`}
+          >
+            Home
+          </a>
+        </Link>
+        <Link href="/about">
+          <a
+            className={[
+              styles.link,
+              router.pathname === "/about" ? styles.active : "",
+            ].join(" ")}
+          >
+            About
+          </a>
+        </Link>
+      </div>
     </nav>
   );
 }
